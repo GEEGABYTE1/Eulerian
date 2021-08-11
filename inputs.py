7edb8389d6a0f1dc0d17ce5fc9a0cf91d71d49ed
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-10 22:37:33 -0400

    Second graph

--- inputs.py
+++ inputs.py
@@ -26,16 +26,23 @@ g1.add_edge(zero, two)
 g1.add_edge(two, one)
 g1.add_edge(zero, three)
 g1.add_edge(three, four)
-g1.add_edge(one, three)
+#g1.add_edge(one, three)
 
 
 # Second Graph 
 
+zero_two = Vertex(0)
+one_two = Vertex(1)
+two_two = Vertex(2)
+three_two = Vertex(3)
+four_two = Vertex(4)
+five_two = Vertex(5)
+
 g2 = Graph()
-g2.add_vertex(zero)
-g2.add_vertex(one)
-g2.add_vertex(two)
-g2.add_vertex(three)
-g2.add_vertex(four)
+g2.add_vertex(zero_two)
+g2.add_vertex(one_two)
+g2.add_vertex(two_two)
+g2.add_vertex(three_two)
+g2.add_vertex(four_two)
 
 
